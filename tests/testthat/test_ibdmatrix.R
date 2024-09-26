ce68c4b0da618b4e543230c89fd198998768e1a2
LouisBzh <louislenezet@gmail.com>
2024-09-26 17:01:16 +0200

    Add test ibdmatrix

---
+++ tests/testthat/test_ibdmatrix.R
@@ -0,0 +1,25 @@
+test_that("ibd_matrix works", {
+    data("relped")
+    df <- data.frame(
+        id1 = c("1", "2", "1", "1"),
+        id2 = c("2", "3", "4", "2"),
+        ibd = c(0.5, 0.16, 0.27, 0.4)
+    )
+    idmap <- data.frame(
+        id = c("1", "2", "3", "4"),
+        name = c("A", "B", "C", "D")
+    )
+    expect_snapshot(
+        "ibd_matrix",
+        ibd_matrix(
+            df$id1, df$id2, df$ibd,
+            diagonal = 2, idmap = idmap
+        )
+    )
+    expect_error(ibd_matrix(df[c("id1", "id2")]))
+    expect_error(ibd_matrix(df$id1))
+    expect_error(ibd_matrix(df$id1, df$id2))
+    expect_error(ibd_matrix(df, df$id2, df$ibd))
+    expect_error(ibd_matrix(df, ibd = df$ibd))
+    
+})
