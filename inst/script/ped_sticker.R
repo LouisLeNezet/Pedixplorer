61365a2314d3652aa1404d25f8170fb067890d90
Louis <louislenezet@gmail.com>
2024-10-04 20:03:48 +0200

    Update logo

--- inst/script/ped_sticker.R
+++ inst/script/ped_sticker.R
@@ -8,7 +8,14 @@ library(Pedixplorer)
 data(sampleped)
 
 # Create Pedigree object
-ped1 <- Pedigree(sampleped[sampleped$family == "1", -1])
+ped1 <- Pedigree(
+    sampleped[sampleped$famid == "1", -1],
+)
+
+rel(ped1) <- Rel(obj = "127", id2 = "128", code = "2")
+
+border(ped1)[2, "border"] <- "#8aca25"
+fill(ped1)[2, "fill"] <- "#3fb7db"
 
 # Shrink Pedigree
 ped1trim <- shrink(ped1, max_bits = 12)
@@ -18,39 +25,22 @@ output_dir <- "inst/figures/graph.png"
 png(output_dir,
     width = 1300, height = 900,
     units = "px", bg = "#FFFFFF00", pointsize = 36)
-op <- par(
-    mar = c(5.5, 0.5, 0.5, 0.5), lwd = 3,
-    oma = c(0, 0, 0, 0)
+
+plot(ped1trim$pedObj,
+    cex = 1.5, symbolsize = 1.1, aff_mark = FALSE,
+    mar = c(4, 0.5, 0.5, 0.5), oma = c(0, 0, 0, 0),
+    lwd = 8
 )
 
-lst <- ped_to_plotdf(
-    ped1trim$pedObj, cex = 1.2, symbolsize = 1, aff_mark = FALSE
+par(oma = c(0, 0, 0, 0), mar = c(0.5, 0.5, 0.5, 0.5))
+plot_legend(
+    ped1trim$pedObj, cex = 0.8, add_to_existing = TRUE,
+    leg_loc = c(0.2, 5.5, 4.1, 5), boxw = 0.2, boxh = 0.18,
+    adjx = 1, adjy = 0.12, lwd = 5
 )
-lst$par_usr$usr[3] <- 3.6
-p <- plot_fromdf(lst$df, usr = lst$par_usr$usr,
-    boxw = lst$par_usr$boxw, boxh = lst$par_usr$boxh
-)
-leg_symbolsize <- 0.6
-leg <- ped_to_legdf(ped1trim$pedObj, cex = 0.8,
-    boxw = lst$par_usr$boxw * leg_symbolsize,
-    boxh = lst$par_usr$boxh * leg_symbolsize,
-    adjx = 1, adjy = 0.15
-)
-leg_loc <- c(0.5, 5, 3.8, 4.5)
-leg$leg_df$x0 <- scales::rescale(leg$leg_df$x0,
-    c(leg_loc[1], leg_loc[2])
-)
-leg$leg_df$y0 <- scales::rescale(leg$leg_df$y0,
-    c(leg_loc[3], leg_loc[4])
-)
-clip(leg_loc[1] - 1, leg_loc[2] + 1, leg_loc[3] - 1, leg_loc[4] + 1)
-plot_fromdf(leg$leg_df, add_to_existing = TRUE,
-    boxw = lst$par_usr$boxw * leg_symbolsize,
-    boxh = lst$par_usr$boxh * leg_symbolsize
-)
+dev.off()
+dev.off()
 
-dev.off()
-dev.off()
 par(op)
 
 # Create sticker
@@ -58,8 +48,8 @@ s <- hexSticker::sticker(
     output_dir,
     package = "Pedixplorer", p_size = 50, p_color = "#000000",
     p_x = 1, p_y = 1.5,
-    s_x = 1, s_y = 0.8, s_width = 0.7, s_height = 0.4,
-    h_fill = "#FFFFFF00", h_color = "#3576ac",
+    s_x = 1, s_y = 0.85, s_width = 0.7, s_height = 0.4,
+    h_fill = "#FFFFFF", h_color = "#3792ad",
     filename = "inst/figures/icon_Pedixplorer.png", dpi = 1000
 )
 
