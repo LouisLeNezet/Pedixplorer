f5f776c3f407561aa4237ef15dfe614c3be14a7b
Louis <louislenezet@gmail.com>
2025-02-09 11:46:10 +0100

    Fix argument data_import

--- R/app_data_import.R
+++ R/app_data_import.R
@@ -111,7 +111,7 @@ read_data <- function(
         }
         return(as.data.frame(
             unclass(df),
-            stringsAsFactors = stringsAsFactors
+            stringsAsFactors = strings_as_factors
         ))
     } else {
         return(NULL)
