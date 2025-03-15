9fd73525aafa65c37a90691d539d8b40c9976cfe
Louis <louislenezet@gmail.com>
2025-03-15 19:48:15 +0100

    Fix border size sticker

--- inst/script/ped_sticker.R
+++ inst/script/ped_sticker.R
@@ -1,8 +1,8 @@
 #### Libraries needed ####
-usethis::use_package("dplyr")
-usethis::use_package("hexSticker")
-
+library(dplyr)
+library(hexSticker)
 library(Pedixplorer)
+library(magick)
 
 # Charge necessary data
 data(sampleped)
@@ -41,16 +41,38 @@ plot_legend(
 dev.off()
 dev.off()
 
-par(op)
-
 # Create sticker
-s <- hexSticker::sticker(
+hexSticker::sticker(
     output_dir,
     package = "Pedixplorer", p_size = 50, p_color = "#000000",
     p_x = 1, p_y = 1.5,
     s_x = 1, s_y = 0.85, s_width = 0.7, s_height = 0.4,
-    h_fill = "#FFFFFF", h_color = "#3792ad",
-    filename = "inst/figures/icon_Pedixplorer.png", dpi = 1000
+    h_fill = "#FFFFFF", h_color = "#3792ad", h_size = 3,
+    filename = "inst/figures/icon_Pedixplorer.png", dpi = 1000,
+    white_around_sticker = TRUE
 )
 
-plot(s)
+# Load the sticker
+sticker <- image_read("inst/figures/icon_Pedixplorer.png")
+
+# Remove the white background while keeping the full border
+sticker_transp <- sticker %>%
+    image_fill(
+        color = "transparent", refcolor = "white",
+        fuzz = 4, point = "+100+100"
+    ) %>%
+    image_fill(
+        color = "transparent", refcolor = "white",
+        fuzz = 4, point = "+1600+1800"
+    ) %>%
+    image_fill(
+        color = "transparent", refcolor = "white",
+        fuzz = 4, point = "+1600+100"
+    ) %>%
+    image_fill(
+        color = "transparent", refcolor = "white",
+        fuzz = 4, point = "+100+1800"
+    )
+
+# Save the final transparent sticker
+image_write(sticker_transp, path = "inst/figures/icon_Pedixplorer.png")
