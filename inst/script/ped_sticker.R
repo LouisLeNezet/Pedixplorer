e6789e0596834331547a6b040c7b0ba88f745c7e
Louis Le Nezet <louislenezet@gamil.com>
2025-07-15 16:24:31 +0200

    Update logo

--- inst/script/ped_sticker.R
+++ inst/script/ped_sticker.R
@@ -45,9 +45,29 @@ hexSticker::sticker(
     output_dir,
     package = "Pedixplorer", p_size = 50, p_color = "#000000",
     p_x = 1, p_y = 1.5, p_family = "Aller_Rg",
-    s_x = 1, s_y = 0.85, s_width = 0.7, s_height = 0.4,
+    s_x = 1, s_y = 0.8, s_width = 0.7, s_height = 0.4,
     h_fill = "#FFFFFF", h_color = "#3792ad", h_size = 3,
     filename = "inst/figures/icon_Pedixplorer.png", dpi = 1000,
+    white_around_sticker = TRUE
+)
+
+hexSticker::sticker(
+    output_dir,
+    package = "Pedixplorer", p_size = 5, p_color = "#000000",
+    p_x = 1, p_y = 1.5, p_family = "Aller_Rg",
+    s_x = 1, s_y = 0.8, s_width = 0.7, s_height = 0.4,
+    h_fill = "#FFFFFF", h_color = "#3792ad", h_size = 3,
+    filename = "inst/figures/icon_Pedixplorer.svg", dpi = 1000,
+    white_around_sticker = TRUE
+)
+
+hexSticker::sticker(
+    output_dir,
+    package = "Pedixplorer", p_size = 5, p_color = "#000000",
+    p_x = 1, p_y = 1.5, p_family = "Aller_Rg",
+    s_x = 1, s_y = 0.8, s_width = 0.7, s_height = 0.4,
+    h_fill = "#FFFFFF", h_color = "#3792ad", h_size = 3,
+    filename = "inst/figures/icon_Pedixplorer.pdf", dpi = 1000,
     white_around_sticker = TRUE
 )
 
